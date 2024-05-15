d43a5d7e2cb5653246c4b36a3e5ee1e712ad62eb
Muhammad Elsaeed <muhammadalsaied96@gmail.com>
2024-05-15 05:37:19 +0300

    Change back to owner

--- iam.tf
+++ iam.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "github_actions_runner" {
 
 resource "google_project_iam_member" "github_actions_runner-project-owner" {
   project = var.project_id
-  role    = "roles/editor"
+  role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.github_actions_runner.email}"
 }
 
