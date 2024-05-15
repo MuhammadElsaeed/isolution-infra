227635ee830ad828185cb776b8ecb902007d67c6
Muhammad Elsaeed <muhammadalsaied96@gmail.com>
2024-05-15 03:44:49 +0300

    Change Runenr SA permssions to owner

--- iam.tf
+++ iam.tf
@@ -2,11 +2,9 @@ resource "google_service_account" "github_actions_runner" {
   account_id   = "github-actions-runners"
 }
 
-resource "google_project_iam_binding" "artifact_registry_binding" {
-  project = var.project_id
-  role    = "roles/artifactregistry.writer"
 
-  members = [
-    "serviceAccount:${google_service_account.github_actions_runner.email}"
-  ]
+resource "google_project_iam_member" "github_actions_runner-project-owner" {
+  project = var.project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.github_actions_runner.email}"
 }
